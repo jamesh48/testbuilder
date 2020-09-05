ac001a9b5581e5b077119fb709b72f18a945de44
James Hrivnak <jameshrivnak4@gmail.com>
2020-09-05 10:36:29 -0600

    Wrote conditional statement for Switch, handling edge cases for Visa using '49' and Maestro using cardNumber.slice(0, 4) and cardNumber.slice(0, 6)

--- detectNetwork.js
+++ detectNetwork.js
@@ -9,17 +9,19 @@
 
 var detectNetwork = function(cardNumber) {
   let twoDigits = cardNumber.slice(0, 2);
-  //China UnionPay
-  if (twoDigits === '62' && (cardNumber.length === 16 || cardNumber.length === 18 || cardNumber.length === 19)) {
+
+  //Switch
+  if ((twoDigits === '49' || twoDigits === '56' || twoDigits === '67' || twoDigits + '33' === cardNumber.slice(0, 4) || twoDigits + '3110' === cardNumber.slice(0, 6)) && (cardNumber.length === 16 || cardNumber.length === 18 || cardNumber.length === 19)) {
+    return 'Switch';
+  } else if (twoDigits === '62' && (cardNumber.length === 16 || cardNumber.length === 18 || cardNumber.length === 19)) {
     return 'China UnionPay';
   } else if ((cardNumber.length >= 12 && cardNumber.length <= 19) && (twoDigits === '63' || twoDigits === '50')) {
     return 'Maestro';
-  } else if ((twoDigits === '60' || twoDigits === '64' || twoDigits === '65') && (cardNumber.length === 16 || cardNumber.length === 19 )) {
+  } else if ((twoDigits === '60' || twoDigits === '64' || twoDigits === '65') && (cardNumber.length === 16 || cardNumber.length === 19)) {
     return 'Discover';
-  }
-  if (twoDigits[0] === '4' && (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)) {
+  } else if (twoDigits[0] === '4' && (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)) {
     return 'Visa';
-  } else if (twoDigits[0] === '5' && cardNumber.length === 16) {
+  } else if ((twoDigits === '51' || twoDigits === '52' || twoDigits === '53' || twoDigits === '54' || twoDigits === '55') && cardNumber.length === 16) {
     return 'MasterCard';
   } else if (cardNumber.length === 14 && (twoDigits === '38' || twoDigits === '39')) {
     return 'Diner\'s Club';
@@ -28,6 +30,10 @@ var detectNetwork = function(cardNumber) {
   } else {
     return 'error';
   }
+
+  //Overlaps with Maestro -63
+  //Overlaps with Visa -4
+  //Length 16, 18, 19
 
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
